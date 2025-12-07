f246aa78ecdce80b554c9a320441cd0447559641
adridiazvic <adriandv91@gmail.com>
2025-12-07 17:12:20 +0100

    Resolve challenge "IMC"

--- bin/imc/imc.dart
+++ bin/imc/imc.dart
@@ -4,6 +4,28 @@ Crea una calculadora del
 índice de masa corporal.
 */
 
+import 'dart:io';
+
 void main(List<String> args) {
-  
+  try {
+    print("Enter your weight in Kg");
+    double weight = double.parse(stdin.readLineSync()!);
+    print("Enter your height in cm");
+    double height = double.parse(stdin.readLineSync()!);
+
+    height /= 100;
+
+    double result = weight / (height * height);
+
+    print(
+      "${result.toStringAsFixed(2)}${switch (result) {
+        < 18.5 => ": Underweight",
+        < 25 => ": Normal weight",
+        < 30 => ": Overweight",
+        _ => ": Obesity",
+      }}",
+    );
+  } catch (e) {
+    print("Invalid entry");
+  }
 }
