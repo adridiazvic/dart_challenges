2aac2207d91c23a164288eaa7015f1cb7b39e2a8
adridiazvic <adriandv91@gmail.com>
2025-12-07 16:09:07 +0100

    Resolve challenge "Vowels Counter"

--- bin/vowels_counter/vowels_counter.dart
+++ bin/vowels_counter/vowels_counter.dart
@@ -4,6 +4,21 @@ Crea un programa que cuente cuantas
 vocales tiene una cadena de texto.
 */
 
+import 'dart:convert';
+import 'dart:io';
+
 void main(List<String> args) {
-  
+  String? input = stdin.readLineSync(encoding: utf8);
+
+  if (input == null) return;
+
+  input = input.toLowerCase();
+  String vowels = "aáeéiíoóuú";
+  int counter = 0;
+
+  for (var i = 0; i < input.length; i++) {
+    if (vowels.contains(input[i])) counter++;
+  }
+
+  print("Number of vowels: $counter");
 }
