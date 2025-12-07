b2f6d81e188a88b6f2575873b1098313339b6e72
adridiazvic <adriandv91@gmail.com>
2025-12-07 14:19:04 +0100

    Resolve challenge "Even Odd"

--- bin/even_odd/even_odd.dart
+++ bin/even_odd/even_odd.dart
@@ -4,6 +4,13 @@ Crea un programa que compruebe si
 un número entero es par o impar.
 */
 
+import 'dart:io';
+
 void main(List<String> args) {
-  
+  try {
+    int number = int.parse(stdin.readLineSync()!);
+    print(number % 2 == 0 ? "It's even" : "It's odd");
+  } catch (e) {
+    print("Invalid entry");
+  }
 }
