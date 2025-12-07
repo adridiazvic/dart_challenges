b13ad62511d15d314d4a268dda27593653570727
adridiazvic <adriandv91@gmail.com>
2025-12-07 13:34:58 +0100

    Resolve challenge Fizzbuzz

--- bin/fizzbuzz/fizzbuzz.dart
+++ bin/fizzbuzz/fizzbuzz.dart
@@ -14,5 +14,15 @@ la palabra "fizzbuzz".
 */
 
 void main(List<String> args) {
-  
+  for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      print("fizzbuzz");
+    } else if (i % 3 == 0) {
+      print("fizz");
+    } else if (i % 5 == 0) {
+      print("buzz");
+    } else {
+      print(i);
+    }
+  }
 }
